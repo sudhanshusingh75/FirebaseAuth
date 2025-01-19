e35bdae69a7717ce0e41f94e3bfe28058c3e22c7
Sudhanshu Kumar <sudhanshukumar07777@gmail.com>
2025-01-19 15:06:51 +0530

    Initial Commit

---
+++ FireBaseAuthTutorail/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FireBaseAuthTutorail
+//
+//  Created by Sudhanshu Singh Rajput on 19/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FireBaseAuthTutorail/FireBaseAuthTutorailApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBaseAuthTutorailApp.swift
+//  FireBaseAuthTutorail
+//
+//  Created by Sudhanshu Singh Rajput on 19/01/25.
+//
+
+import SwiftUI
+
+@main
+struct FireBaseAuthTutorailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
